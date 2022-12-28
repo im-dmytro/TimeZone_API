1d564e10a99225c735aaaebc72569960e4320f14
im-dmytro <dmitri571ivanov571@gmail.com>
2022-12-28 15:43:07 +0200

    added connection string and ef packages

---
+++ TimeZoneAPI/Country.cs
@@ -0,0 +1,13 @@
+﻿namespace TimeZoneAPI
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Capital { get; set; } = string.Empty;
+        public string Code { get; set; } = string.Empty;
+        public string TimeZone { get; set; } = string.Empty;
+        public string GmtOffset { get; set; } = string.Empty;
+        public string FlagUrl { get; set; } = string.Empty;
+    }
+}
--- TimeZoneAPI/Program.cs
+++ TimeZoneAPI/Program.cs
@@ -1,7 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -15,7 +13,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 
 
 
